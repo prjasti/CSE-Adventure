14e4f44af55d008d72e09fa66c63c082274893e6
zsglasser <zaglasser@ctemc.org>
2018-02-23 14:19:30 -0500

    grid

--- grid.py
+++ grid.py
@@ -1,6 +1,9 @@
 from tkinter import *
 master = Tk()
+master2 = Tk()
 w = Canvas(master, width=3000, height=2000)
+d = Canvas(master2, width=300, height=200)
+d.pack()
 w.pack()
 squareSize = 75     #Size of each square
 loc = [225,225]     #Starting point of the first rectangle, used to keep track of location
@@ -20,16 +23,29 @@ def moveDown():
     if loc[1] < 675:   #At extreme bottom of board
         w.move(r,0,squareSize)
         loc[1] += squareSize
+def enter():
+    d = Canvas(master2, width=300, height=200)
+    w.pack()
+    squareSize = 75;
+    loc2 = [225, 225]
+    
 rightBtn = Button(master, text="Move Right", width=10, command = moveRight)
 leftBtn = Button(master, text="Move Left", width=10, command = moveLeft)
 upBtn = Button(master, text="Move Up", width=10, command = moveUp)
 downBtn = Button(master, text="Move Down", width=10, command = moveDown)
-rightBtn.place(x = 1200, y = 400)
-leftBtn.place(x = 1050, y = 400)
+enterBtn = Button(master, text="Enter", width=10, command = enter)
+rightBtn.place(x = 1250, y = 400)
+leftBtn.place(x = 1000, y = 400)
 upBtn.place(x = 1125, y = 300)
 downBtn.place(x = 1125, y = 500)
+enterBtn.place(x = 1125, y = 400)
 for a0 in range(225,750,squareSize):#Creates 49 squares in a grid each of size 75
     for b0 in range(225,750,squareSize):
         w.create_rectangle(a0,b0,a0+65,b0+65, fill="white", outline = "red")
+r = w.create_oval(loc[0]+10,loc[1]+10,loc[0]+55,loc[1] + 55, fill = "chartreuse")
+w.mainloop()
+for a0 in range(225,750,squareSize):#Creates 49 squares in a grid each of size 75
+    for b0 in range(225,750,squareSize):
+        d.create_rectangle(a0,b0,a0+65,b0+65, fill="white", outline = "red")
 r = w.create_oval(loc[0]+10,loc[1]+10,loc[0]+55,loc[1] + 55, fill = "chartreuse")
 w.mainloop()

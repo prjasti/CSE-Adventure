87fe5b831574cf27bd6c9f1bc91c08e60ec17047
prjasti <prjasti@ctemc.org>
2018-03-04 15:25:42 -0500

    Update grid.py

--- grid.py
+++ grid.py
@@ -67,6 +67,44 @@ def displayBackpack():
 def enter():
     if not rooms[currentRoom[0]-1].lightsOn:
         print("This simulation is offline. Get to the center to restart it.")
+    if currentRoom[0] == 10 and rooms[9].visited == False:
+        i = raw_input("What is the smallest number that can be written as the sum \nof two distinct perfect squares in two different ways?")
+        if int(i) != 65:
+            print("Incorrect.")
+        else:
+            print("Correct. Simulation can now be accessed.")
+            rooms[9].visited = True
+    if currentRoom[0] == 33 and rooms[32].visited == False:
+        i = raw_input("What is the fourth smallest number that can be written as the sum \nof two distinct perfect cubes in two different ways?\nNote: this one can not be solved by hand.")
+        if int(i) != 20683:
+            print("Incorrect.")
+        else:
+            print("Correct. Simulation can now be accessed.")
+            rooms[32].visited = True
+    if currentRoom[0] == 18 and rooms[17].visited == False:
+        i = raw_input("What are the last two nonzero digits of 90!(90 factorial)? Surprisingly, this one can be solved by hand.")
+        if int(i) != 12:
+            print("Incorrect.")
+        else:
+            print("Correct. Simulation can now be accessed.")
+            rooms[17].visited = True
+    if currentRoom[0] == 8 and rooms[7].visited == False:
+        i = raw_input("Gie Lovos drada kee. Drass, drageh.: Ikelos, Carrhae, Exigent, Midnight")
+        if i != "Ikelos":
+            print("Urusno ka drasgu.")
+        else:
+            print("Correct. Simulation can now be accessed.")
+            rooms[7].visited = True
+    if currentRoom[0] == 48 and rooms[47].visited == False:
+        i = raw_input("The data carried by this simulation has been corrupted by Argos. You may enter \nit, but for an ultimate price. Give me all your items, and I shall fix it for you.\n Y/N")
+        if i == "N":
+            print("You have made a wise choice. You can not begin to comprehend what is behind there.")
+        else:
+            print("You wish to be a fool. Very well. I can grant you one \nmystical weapon forged by our scientists in exchange for your arsenal.\n")
+            del player.backpack[:]
+            player.backpack.append(Item("Outbreak Prime","Weapon",300,0,95,0,50))
+            print("You have picked up the Outbreak Prime and can now \nchannel the power of the nanite virus against the splicers.")
+            rooms[47].visited = True   
     else:
         if currentRoom[0] == 25:
             print("All offline simulations are now restarted.")
@@ -88,6 +126,7 @@ def enter():
                 rooms[currentRoom[0]-1].furniture.remove((rooms[currentRoom[0] - 1].furniture[taken-1]))
             else:
                 print("Your inventory is full. Get a modification to add more space or drop an item.")
+        rooms[currentRoom[0]-1].visited = True
 rightBtn = Button(master, text="Move Right", width=10, command = moveRight)
 leftBtn = Button(master, text="Move Left", width=10, command = moveLeft)
 upBtn = Button(master, text="Move Up", width=10, command = moveUp)
